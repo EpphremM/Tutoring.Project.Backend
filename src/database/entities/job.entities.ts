b2b2a29bd0c14b99f5fe292078e310953032eb88
EpphremM <mandefroephrem48@gmail.com>
2024-11-20 15:54:39 -0500

    adding relationship between family and job entity

--- src/database/entities/job.entities.ts
+++ src/database/entities/job.entities.ts
@@ -1,56 +1,69 @@
-import 'reflect-metadata'
+import "reflect-metadata";
 import { JobInterface } from "../interfaces/job.interface";
-import { Entity,PrimaryGeneratedColumn,Column, Decimal128, IntegerType, CreateDateColumn, OneToOne, JoinColumn, OneToMany, Transaction, ManyToMany, JoinTable, ManyToOne } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Decimal128,
+  IntegerType,
+  CreateDateColumn,
+  OneToOne,
+  JoinColumn,
+  OneToMany,
+  Transaction,
+  ManyToMany,
+  JoinTable,
+  ManyToOne,
+} from "typeorm";
 import { Application } from "./application.entity";
 import { Tutor } from "./tutor.entity";
-import { Family } from './family.entity';
-import { ExperienceLevel, JobStatus } from '../enum/job.enums';
-import { JobGender } from '../enum/job.enums';
-import { EducationLevel } from '../enum/education.enum';
-@Entity('jobs')
-export class Job implements JobInterface{
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-    @Column({default:"Job Opportunity for Tutor Position"})
-    title: string;
-    @Column({
-        type:"enum",
-        enum:JobStatus
-    })
-    status: JobStatus;
-    @Column()
-    duration: number
-    @Column()
-    hourlyBudget: number
-    @Column({
-        type:"enum",
-        enum:JobGender
-    })
-    requiredGender: JobGender;
-    @Column()
-    weeklyFrequency: number;
-    @Column()
-    workPeriod: String;
-    @Column()
-    startingDate: Date;
-    @Column({type:"enum",
-        enum:ExperienceLevel
-    })
-    experience: ExperienceLevel;
-    @Column()
-    responsibility: string;
-    @Column({type:"enum",
-        enum:EducationLevel
-    })
-    educationLevel: EducationLevel;
-    @CreateDateColumn()
-    createdAt: Date;
-    @ManyToOne(()=>Family,(family)=>family.jobs)
-    family:Family
-    @OneToMany(()=>Application,(application)=>application.job)
-    application:Application[];
+import { Family } from "./family.entity";
+import { ExperienceLevel, JobStatus } from "../enum/job.enums";
+import { JobGender } from "../enum/job.enums";
+import { EducationLevel } from "../enum/education.enum";
+@Entity("jobs")
+export class Job implements JobInterface {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+  @Column({ default: "Job Opportunity for Tutor Position" })
+  title: string;
+  @Column({
+    type: "enum",
+    enum: JobStatus,
+  })
+  status: JobStatus;
+  @Column()
+  duration: number;
+  @Column()
+  hourlyBudget: number;
+  @Column({
+    type: "enum",
+    enum: JobGender,
+  })
+  requiredGender: JobGender;
+  @Column()
+  weeklyFrequency: number;
+  @Column()
+  workPeriod: String;
+  @Column()
+  startingDate: Date;
+  @Column({ type: "enum", enum: ExperienceLevel })
+  experience: ExperienceLevel;
+  @Column()
+  responsibility: string;
+  @Column({ type: "enum", enum: EducationLevel })
+  educationLevel: EducationLevel;
+  @CreateDateColumn()
+  createdAt: Date;
+  @ManyToOne(() => Family, (family) => family.jobs)
+  @JoinColumn({ name: "family_id" })
+  family: Family;
+  @Column()
+  family_id: string;
+  @OneToMany(() => Application, (application) => application.job)
+  application: Application[];
 
-    @ManyToMany(()=>Tutor,(tutor)=>tutor.job)
-    @JoinTable({name:"tutor_job"})
-    tutor:Tutor[]
+  @ManyToMany(() => Tutor, (tutor) => tutor.job)
+  @JoinTable({ name: "tutor_job" })
+  tutor: Tutor[];
 }
--- src/database/repositories/family.repository.ts
+++ src/database/repositories/family.repository.ts
@@ -3,16 +3,17 @@ import { Tutor } from "../entities/tutor.entity";
 import { FamilyInterface } from '../interfaces/family.interface';
 import { AppDataSource } from "../data.source";
 import { Family } from '../entities/family.entity';
+import { Job } from "../entities/job.entities";
 export class FamilyRepository{
     familyRepository=AppDataSource.getRepository<FamilyInterface>(Family);
     async register(family:FamilyInterface){
         return await this.familyRepository.save(family);
     }
     async find (){
-        return await this.familyRepository.find();
+        return await this.familyRepository.find({relations:['jobs']});
     }
     async findById(id:string){
-        return await this.familyRepository.findOne({where:{id}});
+        return await this.familyRepository.findOne({where:{id},relations:['jobs']});
     }
     async update(family:FamilyInterface,newFamily:Partial<FamilyInterface>){
       const updated= await this.familyRepository.merge(family,newFamily);
