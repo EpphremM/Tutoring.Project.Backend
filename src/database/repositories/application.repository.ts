f4a42d765f232afcfda3c424162e90d4286a6eea
EpphremM <mandefroephrem48@gmail.com>
2024-11-24 11:29:32 -0500

    adding repository for application

---
+++ src/database/repositories/application.repository.ts
@@ -0,0 +1,28 @@
+import { AppDataSource } from "../data.source";
+import { Application } from "../entities/application.entity";
+import { ApplicationInterface } from "../interfaces/application.interface";
+
+export class ApplicationRepository{
+    applicationRepository=AppDataSource.getRepository<ApplicationInterface>(Application);
+    static appRepo:ApplicationRepository|null=null;
+    private constructor(){}
+   async registration(application:ApplicationInterface){
+    return await  this.applicationRepository.save(application);
+    }
+  async find(){
+    return await this.applicationRepository.find();
+  }
+async update(application:ApplicationInterface,newApplication:ApplicationInterface){
+    const updated:ApplicationInterface=await this.applicationRepository.merge(application,newApplication);
+    return await this.applicationRepository.save(updated);
+}
+async findById(id:string){
+    return await this.applicationRepository.findBy({id})
+}
+static getRepo(){
+    if(!ApplicationRepository.appRepo){
+      ApplicationRepository.appRepo=ApplicationRepository.appRepo;
+    }
+    return ApplicationRepository.appRepo;
+}
+}
